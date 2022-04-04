7419b0ff84f7910c3389955352e5d406a89e22b4
Damian <pk-14@sr.dmcs.pl>
2022-04-04 15:09:14 +0200

    Add order service

---
+++ src/main/java/com/ZAI/demo/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ZAI.demo.repository;
+
+import com.ZAI.demo.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
