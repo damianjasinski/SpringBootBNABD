07902b7e8e62c965c53e3ce96380e7e41c1a2052
Damian <pk-14@sr.dmcs.pl>
2022-04-04 15:09:43 +0200

    Add seat repository

---
+++ src/main/java/com/ZAI/demo/services/SeatService.java
@@ -0,0 +1,37 @@
+package com.ZAI.demo.services;
+
+
+import com.ZAI.demo.exceptions.NotFoundException;
+import com.ZAI.demo.models.Order;
+import com.ZAI.demo.models.Seat;
+import com.ZAI.demo.repository.SeatRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+@Data
+public class SeatService {
+    private final OrderService orderService;
+    private final SeatRepository seatRepository;
+
+    public void test(Seat seat, Order order, long seanceId ) {
+        Set<Order> orderSet = seat.getOrderSet();
+        List<Order> isReserved = orderSet.stream()
+                .filter((x) -> x.getSeance().getId() >= 0)
+                .collect(Collectors.toList());
+        if (isReserved.isEmpty()) {
+            orderSet.add(order);
+            seat.setOrderSet(orderSet);
+        } else {
+            throw new NotFoundException("Seat is already reserverd on this seance");
+        }
+    }
+
+
+}
