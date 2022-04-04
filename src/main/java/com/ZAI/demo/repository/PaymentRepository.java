cca40fe645d79c5857d238f387c7b4d33027d13d
Damian <pk-14@sr.dmcs.pl>
2022-04-04 15:09:29 +0200

    Add payment service

---
+++ src/main/java/com/ZAI/demo/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.ZAI.demo.repository;
+
+import com.ZAI.demo.models.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
