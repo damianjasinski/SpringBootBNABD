75c43bb362ebd60889be7b49ab9b82c561126696
Damian <pk-14@sr.dmcs.pl>
2022-04-05 15:29:22 +0200

    Add order controller

---
+++ src/main/java/com/ZAI/demo/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.ZAI.demo.controller;
+
+import com.ZAI.demo.models.Order;
+import com.ZAI.demo.models.Payment;
+import com.ZAI.demo.services.OrderService;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Data
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping("/put")
+    public ResponseEntity<Map<String, String>> addOrder(@Valid @RequestBody Order order) {
+        orderService.addNewOrder(order);
+        return new ResponseEntity<>(Map.of("success", "true"), HttpStatus.OK);
+    }
+
+}
