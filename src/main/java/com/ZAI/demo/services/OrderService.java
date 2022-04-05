0c62b243c0b07d08f7a08c6dd20a23aef4562051
Damian <pk-14@sr.dmcs.pl>
2022-04-05 15:30:13 +0200

    Remove payment from method argument, add auto generate created date

--- src/main/java/com/ZAI/demo/services/OrderService.java
+++ src/main/java/com/ZAI/demo/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.ZAI.demo.services;
 
 
+import com.ZAI.demo.exceptions.NotFoundException;
 import com.ZAI.demo.models.Order;
 import com.ZAI.demo.models.Payment;
 import com.ZAI.demo.repository.OrderRepository;
@@ -9,21 +10,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Data
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
-
+    private final PaymentService paymentService;
 
 
     //TODO check if user id is equals to logged user id (user a cant order for user b)
     //check if payment is finalized
-    public void generateOrder(Order order, Payment payment) {
+    public void addNewOrder(Order order) {
+        Payment payment = order.getPayment();
         if (payment.isFinalized()) {
-            orderRepository.save(order);
+            order.setCreatedAt(LocalDate.now());
+            payment.setOrder(order);
+            Order saved = orderRepository.save(order);
+            paymentService.addPayment(payment);
         }
+        else throw new NotFoundException("You must finalize payment");
     }
 
 
