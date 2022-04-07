b36cad126d56e104541c5cb99be3a98dd65ec912
damian <damian.kania94@gmail.com>
2022-04-07 19:17:17 +0200

    Add category mapping in TitlesService in add method

--- src/main/java/com/ZAI/demo/services/TitlesService.java
+++ src/main/java/com/ZAI/demo/services/TitlesService.java
@@ -2,20 +2,36 @@ package com.ZAI.demo.services;
 
 
 import com.ZAI.demo.exceptions.NotFoundException;
+import com.ZAI.demo.models.Category;
 import com.ZAI.demo.models.Titles;
+import com.ZAI.demo.repository.CategoryRepository;
 import com.ZAI.demo.repository.TitlesRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
 public class TitlesService {
     private final TitlesRepository titlesRepository;
+    private final CategoryRepository categoryRepository;
 
     public void addTitles(Titles titles){
-        titlesRepository.save(titles);
+        List<Category> categories = categoryRepository.findAllById(titles.getCategoriesId());
+
+        if (categories.size() != titles.getCategoriesId().size()){
+            throw new NotFoundException("Category not found");
+        }else{
+            titles.setCategorySet(Set.copyOf(categories));
+            titlesRepository.save(titles);
+        }
+
+        System.out.println(titles.getCategoriesId());
+
     }
 
     public void removeTitles(String name){
