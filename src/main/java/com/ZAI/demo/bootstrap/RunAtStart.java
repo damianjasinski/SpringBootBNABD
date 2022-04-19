36743ba51d6a1847f09c6340da0140579b391677
Damian <you@example.com>
2022-04-19 22:48:06 +0200

    Add few users for testing purposes

--- src/main/java/com/ZAI/demo/bootstrap/RunAtStart.java
+++ src/main/java/com/ZAI/demo/bootstrap/RunAtStart.java
@@ -5,12 +5,10 @@ import com.ZAI.demo.models.Users;
 import com.ZAI.demo.repository.PaymentCardRepository;
 import com.ZAI.demo.repository.UsersRepository;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Component
 @Data
@@ -25,6 +23,12 @@ public class RunAtStart {
         Users user1 = new Users(null, "Pankracy", "$2a$12$tQdopFPcVGF5h5VQbf3fAuaZHCoP1s6pXNR6EhOARxU3Hr4xNWXVu", LocalDate.now(),
                 "pankracy@gmail.com", "USER", null, null);
         usersRepository.save(user1);
+        Users user2 = new Users(null, "Pankraxcy", "$2a$12$tQdopFPcVGF5h5VQbf3fAuaZHCoP1s6pXNR6EhOARxU3Hr4xNWXVu", LocalDate.now(),
+                "pankrdacy@gmail.com", "USER", null, null);
+        usersRepository.save(user2);
+        Users user3 = new Users(null, "panek", "$2a$12$tQdopFPcVGF5h5VQbf3fAuaZHCoP1s6pXNR6EhOARxU3Hr4xNWXVu", LocalDate.now(),
+                "panek@gmail.com", "ADMIN", null, null);
+        usersRepository.save(user3);
 
         PaymentCard paymentCard = new PaymentCard("1234123412341234", LocalDate.now(), "334", user1, null);
         paymentCardRepository.save(paymentCard);
