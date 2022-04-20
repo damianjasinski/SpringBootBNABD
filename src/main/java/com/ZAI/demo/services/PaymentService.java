544a4aa47079c19542fdf2c3bef7ec6dc04d4a0f
Damian <pk-14@sr.dmcs.pl>
2022-04-20 23:01:52 +0200

    Add get user payments method

--- src/main/java/com/ZAI/demo/services/PaymentService.java
+++ src/main/java/com/ZAI/demo/services/PaymentService.java
@@ -1,18 +1,25 @@
 package com.ZAI.demo.services;
 
 import com.ZAI.demo.exceptions.NotFoundException;
+import com.ZAI.demo.models.Order;
 import com.ZAI.demo.models.Payment;
 import com.ZAI.demo.repository.PaymentRepository;
+import com.ZAI.demo.repository.UsersRepository;
+import com.ZAI.demo.security.MyUserDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Data
 @AllArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+    private final UsersRepository usersRepository;
 
     public boolean addPayment(Payment payment) {
         payment.setCreatedAt(LocalDate.now());
@@ -23,8 +30,10 @@ public class PaymentService {
         return true;
     }
 
-    //TODO check if user id is equals to loged user id (user a cant show user b payments)
-//    public List<Payment> getPayments(long userId) {
-//
-//    }
+    public List<Payment> getPayments(long userId) {
+        return usersRepository.getById(userId).getOrderSet()
+                .stream()
+                .map(Order::getPayment)
+                .collect(Collectors.toList());
+    }
 }
