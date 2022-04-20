f84b4fe5a91313fc369c65014b1263fe7845d008
Damian <pk-14@sr.dmcs.pl>
2022-04-20 18:13:10 +0200

    Check if user is able to delete card

--- src/main/java/com/ZAI/demo/services/PaymentCardService.java
+++ src/main/java/com/ZAI/demo/services/PaymentCardService.java
@@ -4,9 +4,13 @@ package com.ZAI.demo.services;
 import com.ZAI.demo.exceptions.NotFoundException;
 import com.ZAI.demo.models.PaymentCard;
 import com.ZAI.demo.repository.PaymentCardRepository;
+import com.ZAI.demo.security.MyUserDetails;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -24,10 +28,16 @@ public class PaymentCardService {
     }
 
     public PaymentCard deletePaymentCard(Long cardNumber) {
+        MyUserDetails user = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long currentUserId = user.getId();
         Optional<PaymentCard> checkCard = paymentCardRepository.findById(cardNumber);
-        if (checkCard.isEmpty()) throw new NotFoundException("Card number not found");
-        paymentCardRepository.deleteById(cardNumber);
-        return checkCard.get();
+        if (checkCard.isPresent()) {
+            if (checkCard.get().getUsers().getId() == currentUserId) {
+                paymentCardRepository.deleteById(cardNumber);
+                return checkCard.get();
+            }
+        }
+        throw new NotFoundException("Card not found");
     }
 
     public PaymentCard getPaymentCard(Long cardNumber) {
