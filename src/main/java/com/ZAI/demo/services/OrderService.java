c169dcfd2d534f614a5a6fb5c4f3867bbcd6eac3
Damian <pk-14@sr.dmcs.pl>
2022-04-20 23:01:31 +0200

    Add get user orders method

--- src/main/java/com/ZAI/demo/services/OrderService.java
+++ src/main/java/com/ZAI/demo/services/OrderService.java
@@ -4,14 +4,16 @@ package com.ZAI.demo.services;
 import com.ZAI.demo.exceptions.NotFoundException;
 import com.ZAI.demo.models.Order;
 import com.ZAI.demo.models.Payment;
+import com.ZAI.demo.models.Seat;
 import com.ZAI.demo.repository.OrderRepository;
-import com.ZAI.demo.repository.PaymentRepository;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.ZAI.demo.repository.UsersRepository;
+import com.ZAI.demo.security.MyUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -19,23 +21,31 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final PaymentService paymentService;
+    private final SeatService seatService;
+    private final UsersRepository usersRepository;
 
-
-    //TODO check if user id is equals to logged user id (user a cant order for user b)
-    //TODO seat reservation
-    //check if payment is finalized
     public Order addNewOrder(Order order) {
-        Payment payment = order.getPayment();
-        if (payment.isFinalized()) {
-            order.setCreatedAt(LocalDate.now());
-            payment.setOrder(order);
-            Order saved = orderRepository.save(order);
-            paymentService.addPayment(payment);
-            return saved;
+        if (order.getUsers().getId() == ((MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId()) {
+            Payment payment = order.getPayment();
+            if (payment.isFinalized()) {
+                //reserve each seat
+                for (Seat seat : order.getSeatSet()) {
+                    seatService.reserveSeat(seat, order);
+                }
+                order.setCreatedAt(LocalDate.now());
+                payment.setOrder(order);
+                Order saved = orderRepository.save(order);
+                paymentService.addPayment(payment);
+                return saved;
+            }
+            else throw new NotFoundException("You must finalize payment");
         }
-        else throw new NotFoundException("You must finalize payment");
+        throw new NotFoundException("You can't order for other user!");
     }
 
+    public Set<Order> getOrders(long userId) {
+        return usersRepository.getById(userId).getOrderSet();
+    }
 
 
 }
